370e7399209a0ba1b228f7df3a5aeb56d0b54df9
Shajal Ahamed <shajalahamed@Shajals-MacBook-Pro.local>
2018-12-28 21:51:29 +0600

    Basic algorithm is working

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 "use strict"
 
-function randomElement(minimum, maximum){
+function randomInteger(minimum, maximum){
     return Math.floor(Math.random()*(maximum - minimum + 1)) + minimum;
 }
 
+function randomElement(arr) {
+    return arr[randomInteger(0, arr.length - 1)];
+}
     
 /**
  *
  * @param JSON options
  * @return string
- * @throws Exception
+ * 
  */
-function generate(options){
+function generateOne(options){
 
     let length = options.length || 8 ;
     let prefix = options.prefix || '' ;
@@ -24,18 +27,51 @@ function generate(options){
     const lowerCase = "abcdefghijklmnopqrstuvwxyz" ;
     const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
     let code = "";
+    let characters = "";
+
+    if (useLetters){
+        if (useMixedCase) {
+            characters += lowerCase + upperCase ;
+        } 
+        else{
+            characters += upperCase;
+        }
+    }
+
+    if (useNumbers) {
+        characters += numbers ;
+    }
+
 
     for(let index = 0; index < length; index++){
-        console.log(randomElement(0,index))
+        console.log(randomInteger(0,characters.length));
+        code += randomElement(characters);
     }
+    console.log(code);
+    return prefix+code+suffix;
 }
 
 
-function generateCoupons(maxNumberOfCoupons=1, config){
 
+function generateCoupons(config){
+    
+    let count = config.count;
+    let codes = {};
+
+    while (count > 0) {
+        var code = generateOne(config);
+        if (codes[code] === undefined) {
+            codes[code] = true;
+            count--;
+        }
+    }
+
+    return Object.keys(codes);
 }
+
 const config = {length: 3};
-generate(config);
+generateOne(config);
+
 module.exports  = {
-    generateCoupon: generateCoupon
+    generateCoupons: generateCoupons
 }
